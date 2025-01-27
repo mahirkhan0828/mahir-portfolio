5c781647ae39288b74fd05fa2c8eceaca47791a9
Mahir Khan <mahirkhan0828@Mahirs-MBP.lan>
2025-01-27 14:15:24 -0500

    Added new project

--- app/projects/util/projectData.js
+++ app/projects/util/projectData.js
@@ -1,45 +1,58 @@
 export const projects = [
-    {
-      id: '1',
-      title: 'Copycat Instagram',
-      description: 'A full-stack social media web app where users can create posts, follow others, and engage with a dynamic feed.',
-      image: './projectImages/social.jpg',
-      link: 'https://copycat-instagram.vercel.app/',
-    },
-    {
-      id: '2',
-      title: 'Grocery Shopping Cart',
-      description: 'A web application that allows users to search for food items by calorie count and add them to a shopping cart.',
-      image: './projectImages/grocery.jpg',
-      link: 'http://grocerycaloriecounter.surge.sh/',
-    },
-    {
-      id: '3',
-      title: 'Bring An Umbrella Notifier',
-      description: 'Email notifier that scrapes data and warns you on potential rainy days.',
-      image: './projectImages/umbrella.jpg',
-      link: 'https://github.com/mahirkhan0828/Bring-An-Umbrella',
-    },
-    {
-      id: '4',
-      title: 'Genius Quote Machine',
-      description: 'A web application that displays inspirational quotes, allows random generation by person, and lets users save favorites to a "Quote Backpack" for later.',
-      image: './projectImages/brain.jpg',
-      link: 'http://geniusquotemachine.surge.sh/',
-    },
-    {
-        id: '5',
-        title: 'Post-It Advice Forum',
-        description: "A forum web application where users can post advice, create posts, and like others' posts across various categories.",
-        image: './projectImages/stickynotes.jpg',
-        link: 'http://post-it-advice-forum.surge.sh/',
-    },
-    {
-        id: '6',
-        title: 'Personal Portfolio',
-        description: 'This Page!',
-        image: './projectImages/portfolio.png',
-        link: '/'
-    }
-  ];
-  
+  {
+    id: "1",
+    title: "Copycat Instagram",
+    description:
+      "A full-stack social media web app where users can create posts, follow others, and engage with a dynamic feed.",
+    image: "./projectImages/social.jpg",
+    link: "https://copycat-instagram.vercel.app/",
+  },
+  {
+    id: "2",
+    title: "Grocery Shopping Cart",
+    description:
+      "A web application that allows users to search for food items by calorie count and add them to a shopping cart.",
+    image: "./projectImages/grocery.jpg",
+    link: "http://grocerycaloriecounter.surge.sh/",
+  },
+  {
+    id: "3",
+    title: "Bring An Umbrella Notifier",
+    description:
+      "Email notifier that scrapes data and warns you on potential rainy days.",
+    image: "./projectImages/umbrella.jpg",
+    link: "https://github.com/mahirkhan0828/Bring-An-Umbrella",
+  },
+  {
+    id: "4",
+    title: "Genius Quote Machine",
+    description:
+      'A web application that displays inspirational quotes, allows random generation by person, and lets users save favorites to a "Quote Backpack" for later.',
+    image: "./projectImages/brain.jpg",
+    link: "http://geniusquotemachine.surge.sh/",
+  },
+  {
+    id: "5",
+    title: "Translate App",
+    description:
+      "A web application that allows you to translate text in English, French, and Spanish.",
+    image: "./projectImages/globe.jpg",
+    link: "https://translate-app-peach-zeta.vercel.app/",
+  },
+  {
+    id: "6",
+    title: "Post-It Advice Forum",
+    description:
+      "A forum web application where users can post advice, create posts, and like others' posts across various categories.",
+    image: "./projectImages/stickynotes.jpg",
+    link: "http://post-it-advice-forum.surge.sh/",
+  },
+
+  {
+    id: "7",
+    title: "Personal Portfolio",
+    description: "This Page!",
+    image: "./projectImages/portfolio.png",
+    link: "/",
+  },
+];
